f0f0cd9697f55661ff38e62ea23b856ec97b8d93
damianld <damianld@damianpc>
2017-11-19 00:27:27 +0100

    Código final exercicio4

--- Programación/Tema1/Boletín1/Exercicio4/src/Main.java
+++ Programación/Tema1/Boletín1/Exercicio4/src/Main.java
@@ -1,4 +1,26 @@
 
 public class Main {
+	public static void main(String[] args) {
 
+		// ¿Por qué da true?
+		System.out.println(3 + 5 < 5 * 2 || 3 > 8 && 7 > 6 - 2);
+
+		// Vamos a empezar a simplificar
+		// 8<10 || 3>8 && 7>4
+
+		// Cambiamos os intervalos por true ou false
+		// true || false && true
+
+		// Cambiamos true por 1 e false por 0, tamén || por OR e && por AND
+		// 1 OR 0 AND 1
+
+		// mirando a táboa de OR e AND
+		// No caso de OR 1 + 0 = 1
+		// No caso de AND 1 + 1 = 1
+
+		// Resultado = 1
+		// Recordando o código anterior: 1 equivale a true, e 0 a false
+		// Resultado final = true
+
+	}
 }
