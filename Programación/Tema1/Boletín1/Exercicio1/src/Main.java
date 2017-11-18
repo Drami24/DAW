e07ce4f4c8864fa1316e7a8ec357b21a589f514b
damianld <damianld@damianpc>
2017-11-18 22:08:19 +0100

    Exercicio2 + modificación no Exercicio1

--- Programación/Tema1/Boletín1/Exercicio1/src/Main.java
+++ Programación/Tema1/Boletín1/Exercicio1/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//O método println permite imprimir por pantalla a través dunha ventá de consola
+		// O método println permite imprimir por pantalla a través dunha ventá de consola
 		System.out.println("Ola curso");
 	}
 
---
+++ Programación/Tema1/Boletín1/Exercicio2/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+
+		// ***VARIABLES*** por exemplo
+		// Nombre de varialbe: num1
+		// Contenido de la variable: 3
+		// Tipo de variable: número entero
+
+		int num1 = 3;
+		int num2 = 4;
+
+		// Se non engadimos os parénteses o resultado é 34 porque a suma de texto e número concatena
+		System.out.println("Resultado: " + (num1 + num2));
+	}
+}
