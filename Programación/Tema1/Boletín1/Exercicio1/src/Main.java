90b2f39bb3b897876a09c5c316db175bc0645ad3
damianld <damianld@damianpc>
2017-11-18 21:18:38 +0100

    Exercicio1

---
+++ Programación/Tema1/Boletín1/Exercicio1/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//O método println permite imprimir por pantalla a través dunha ventá de consola
+		System.out.println("Ola curso");
+	}
+
+}
