007d8f72150ed4833288d988734c451dac9ecce4
damianld <damilores@gmail.com>
2017-12-13 07:53:19 +0100

    correción na condición grazas a Martín

--- Programación/Tema2/Boletín1/Exercicio2/src/Main.java
+++ Programación/Tema2/Boletín1/Exercicio2/src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int numero = 89;
+		int numero = 86;
 
 		// Condición proposta no enunciado
-		if (numero > 100 || numero % 2 == 0) {
+		if (numero > 100 || numero % 2 != 0) {
 			System.out.println("O número " + numero + " cumpre a condición.");
 		} else {
 			System.out.println("O número " + numero + " non cumpre a condición.");
