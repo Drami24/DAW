3d12cc0cbf94516f0a7e6e5b532fde5fe8dec05b
damianld <damilores@gmail.com>
2017-12-16 16:03:38 +0100

    prueba cambio de nombre

--- Programación/Tema1/Boletín1/Exercicio1/src/Main.java
+++ Programación/Tema1/Boletín1/Exercicio1/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		// O método println() permite imprimir por pantalla a través da consola
-		System.out.println("Ola curso"); // Ola curso
+		System.out.println("Ola cursos"); // Ola curso
 	}
 }
